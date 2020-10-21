5472de666e2d3e848e3d1eaaf372db189ee51df5
Huynh Du Luong <huynhduluong@gmail.com>
2020-10-21 13:32:33 +0700

    layout

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./../node_modules/jquery/dist/jquery.min";
+import "./../node_modules/bootstrap/dist/js/bootstrap.min";
+import "./../node_modules/popper.js/dist/umd/popper.min";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
