964fb8bf129142b2cfb4b1e4925a0f426c02501e
Huynh Du Luong <huynhduluong@gmail.com>
2020-10-26 10:02:24 +0700

    Updates

---
+++ precache-manifest.8b268fd402a5c82fd9e21bd0ac0caf8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae9f5d35a5a779fd0fab2932f053837",
+    "url": "/user-management/index.html"
+  },
+  {
+    "revision": "d99b55382de0a9ce5918",
+    "url": "/user-management/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "2780f07a5652f810d72b",
+    "url": "/user-management/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "d99b55382de0a9ce5918",
+    "url": "/user-management/static/js/2.b5332c0c.chunk.js"
+  },
+  {
+    "revision": "cbf58f127d5d8c61c19ec2facc864357",
+    "url": "/user-management/static/js/2.b5332c0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2780f07a5652f810d72b",
+    "url": "/user-management/static/js/main.a528e216.chunk.js"
+  },
+  {
+    "revision": "0f3280e0bd93dfdb4d1d",
+    "url": "/user-management/static/js/runtime-main.9712d40f.js"
+  }
+]);
